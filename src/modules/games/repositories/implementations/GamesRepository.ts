81bf2b0930e2b70e8ef72b4c5fd3c7b06e67bcdb
Willian Bretzke <willianbretzke2013@hotmail.com>
2023-01-08 10:16:20 -0300

    Finalizado

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -1,9 +1,9 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from "typeorm";
 
-import { User } from '../../../users/entities/User';
-import { Game } from '../../entities/Game';
+import { User } from "../../../users/entities/User";
+import { Game } from "../../entities/Game";
 
-import { IGamesRepository } from '../IGamesRepository';
+import { IGamesRepository } from "../IGamesRepository";
 
 export class GamesRepository implements IGamesRepository {
   private repository: Repository<Game>;
@@ -14,12 +14,12 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(title: string): Promise<Game[]> {
     const game = await this.repository
-    .createQueryBuilder()
-    .select("games")
-    .from(Game, "games")
-    .where(`LOWER(games.title) LIKE LOWER('%${title}%')`)
-    .getMany();
-    
+      .createQueryBuilder()
+      .select("games")
+      .from(Game, "games")
+      .where(`LOWER(games.title) LIKE LOWER('%${title}%')`)
+      .getMany();
+
     return game;
   }
 
@@ -28,9 +28,13 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      .where("id = :id", {id: id})
-      .execute()
+    return this.repository.query(`
+    select * from users u  
+    left join users_games_games ugg 
+    on (ugg."usersId" = u.id)
+    left join games g  
+    on (g.id = ugg."gamesId")
+    where  g.id = '${id}'
+    `); // não consegui usando query builder, apenas com .query
   }
 }
